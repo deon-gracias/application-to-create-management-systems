c97820f5f9c36b6e697bad3361354c3e7279cce6
Anonymous616 <Anonymous616>
2021-05-09 17:38:30 +0530

    Tabs Components Created and Completed

---
+++ src/components/tabs/TabPanel.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Box, Typography } from "@material-ui/core";
+
+export default function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`scrollable-tabpanel-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
