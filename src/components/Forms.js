99c6865c52dabd3484ee16e6681f3f6ee0fdeb32
Anonymous616 <Anonymous616>
2021-05-09 17:41:47 +0530

    Forms Component Completed

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,3 +1,60 @@
-export const Forms = () => {
-  return <></>;
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { Box, Card, Container, Tab, Tabs } from "@material-ui/core";
+import TabPanel from "./tabs/TabPanel";
+import NavbarLayout from "./navbar/NavbarLayout";
+import Form from "./forms/Form";
+
+export const Forms = ({ projectId, projectData, reloadData }) => {
+  const history = useHistory();
+  let tableNames = [],
+    headers = [];
+
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  function segregateData() {
+    if (projectId === "") {
+      history.push("/project");
+    } else {
+      projectData.tables.forEach((table) => {
+        tableNames.push(table.name);
+        headers.push(table.headers);
+      });
+    }
+  }
+
+  return (
+    <Card>
+      {segregateData()}
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        indicatorColor="primary"
+        textColor="primary"
+        variant="scrollable"
+        scrollButtons="auto"
+      >
+        {tableNames.map((name, index) => (
+          <Tab label={name} key={`tab${index}`} />
+        ))}
+      </Tabs>
+      {tableNames.map((name, index) => (
+        <TabPanel value={value} index={index} key={`tab-panel${index}`}>
+          <Box>
+            <Form
+              projectId={projectId}
+              headers={headers[index]}
+              projectData={projectData}
+              tableIndex={index}
+              reloadData={reloadData}
+            />
+          </Box>
+        </TabPanel>
+      ))}
+    </Card>
+  );
 };
