7b00cf69f849eea54c571c5ab2be87c9a5090f5b
Deon Graicas <deongracias1@gmail.com>
2021-05-22 14:27:01 +0530

    Removed bug of hambrurger menu disappearing on big screens

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,16 +1,14 @@
 import PropTypes from "prop-types";
-import { AppBar, Box, Hidden, IconButton, Toolbar } from "@material-ui/core";
+import { AppBar, Box, IconButton, Toolbar } from "@material-ui/core";
 import { Menu as MenuIcon } from "@material-ui/icons/";
 
 const Navbar = ({ onMobileNavOpen, ...rest }) => (
   <AppBar elevation={0} {...rest}>
     <Toolbar>
       <Box sx={{ flexGrow: 1 }} />
-      <Hidden lgUp>
-        <IconButton color="inherit" onClick={onMobileNavOpen}>
-          <MenuIcon />
-        </IconButton>
-      </Hidden>
+      <IconButton color="inherit" onClick={onMobileNavOpen}>
+        <MenuIcon />
+      </IconButton>
     </Toolbar>
   </AppBar>
 );
