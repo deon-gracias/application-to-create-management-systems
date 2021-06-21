0f61259491dc7a30f6eff2b5d20873ada475b98e
Deon Graicas <deongracias1@gmail.com>
2021-06-21 15:41:07 +0530

    Add Project and Delete Project

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,21 +7,17 @@ import {
   Grid,
   Button,
   IconButton,
-  Modal,
-  Backdrop,
-  Fade,
   Toolbar,
-  Typography,
-  TextField,
 } from "@material-ui/core";
 import {
   Delete as DeleteIcon,
   Add as AddIcon,
   Logout as LogoutIcon,
 } from "@material-ui/icons";
-import { Formik } from "formik";
-import * as Yup from "yup";
+
 import { auth } from "../helper/firebase";
+import AddProjectModal from "../components/project/AddProjectModal";
+import DeleteProjectModal from "../components/project/DeleteProjectModal";
 
 export const Project = ({
   projectId,
@@ -33,109 +29,27 @@ export const Project = ({
   const history = useHistory();
 
   const [addProjectOpen, setAddProjectOpen] = useState(false);
-
-  useEffect(() => {
-    auth.onAuthStateChanged((user) => {
-      if (!user) {
-        history.push("/login");
-      }
-    });
-  });
-
-  function AddProjectModal() {
-    return (
-      <Modal
-        open={addProjectOpen}
-        onClose={() => {
-          setAddProjectOpen(false);
-        }}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
-        }}
-      >
-        <Fade in={addProjectOpen}>
-          <Box
-            sx={{
-              position: "absolute",
-              top: "50%",
-              left: "50%",
-              transform: "translate(-50%, -50%)",
-              width: 400,
-              bgcolor: "background.paper",
-              borderRadius: "5px",
-              boxShadow: 24,
-              p: 4,
-              display: "grid",
-              gap: "15px",
-            }}
-          >
-            <Typography id="transition-modal-title" variant="h3">
-              Add Project
-            </Typography>
-            <Formik
-              initialValues={{
-                projectName: "",
-              }}
-              validationSchema={Yup.object().shape({
-                projectName: Yup.string()
-                  .max(255)
-                  .required("Project Name is required"),
-              })}
-              onSubmit={(values) => {
-                addNewProject({
-                  name: values.projectName,
-                  uid: auth.currentUser.uid,
-                  tables: [],
-                });
-                setAddProjectOpen(false);
-                reloadData();
-              }}
-            >
-              {({
-                errors,
-                handleBlur,
-                handleChange,
-                handleSubmit,
-                touched,
-                values,
-              }) => (
-                <form onSubmit={handleSubmit}>
-                  <TextField
-                    error={Boolean(touched.projectName && errors.projectName)}
-                    fullWidth
-                    helperText={touched.projectName && errors.projectName}
-                    label="Project Name"
-                    margin="normal"
-                    name="projectName"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    type="projectName"
-                    value={values.projectName}
-                    variant="outlined"
-                  />
-                  <Button variant="contained" type="submit">
-                    Submit
-                  </Button>
-                </form>
-              )}
-            </Formik>
-          </Box>
-        </Fade>
-      </Modal>
-    );
-  }
+  const [deleteProjectOpen, setDeleteProjectOpen] = useState(false);
 
   return (
     <>
-      <AddProjectModal />
+      <AddProjectModal
+        addProjectOpen={addProjectOpen}
+        addNewProject={addNewProject}
+        setAddProjectOpen={setAddProjectOpen}
+      />
+      <DeleteProjectModal
+        projectsData={projectsData}
+        deleteProjectOpen={deleteProjectOpen}
+        setDeleteProjectOpen={setDeleteProjectOpen}
+        reloadData={reloadData}
+      />
       <AppBar>
         <Toolbar>
           <IconButton onClick={() => setAddProjectOpen(true)}>
             <AddIcon />
           </IconButton>
-          <IconButton>
+          <IconButton onClick={() => setDeleteProjectOpen(true)}>
             <DeleteIcon />
           </IconButton>
           <IconButton
